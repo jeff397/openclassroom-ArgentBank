3e53fabc735f56713bba5868243df229b5c35477
jeff <jeffdelmotte@gmail.com>
2025-01-24 14:20:30 +0100

    implémentation de la section features

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../../components/Hero/hero";
+import Features from "../../components/Features/features";
 
 function Home() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
