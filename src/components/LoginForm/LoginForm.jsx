1fbcabb26b1a9cc37fdb95e788010bb1b5e717c5
jeff <jeffdelmotte@gmail.com>
2025-01-29 11:29:39 +0100

    creation du store redux

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,25 +1,86 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess, loginFail } from "../../Slices/LoginSlice";
+import { LoginCall } from "../../Api/LoginAuth";
 import "./LoginForm.css";
 
 function LoginForm() {
-  return (
-    <form>
-      <div className="input-wrapper">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-      </div>
-      <div className="input-wrapper">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-      </div>
-      <div className="input-remember">
-        <input type="checkbox" id="remember-me" />
-        <label htmlFor="remember-me">Remember me</label>
-      </div>
+  const dispatch = useDispatch();
+  const { error } = useSelector((state) => state.login);
 
-      <a href="./user.html" className="sign-in-button">
-        Sign In
-      </a>
-    </form>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      email,
+      password,
+      rememberMe,
+    };
+
+    try {
+      const data = await LoginCall(formData);
+      const token = data.body.token;
+      console.log("Données retournées par l'API :", data);
+
+      if (rememberMe) {
+        localStorage.setItem("token", token);
+      } else {
+        sessionStorage.setItem("token", token);
+      }
+
+      dispatch(loginSuccess({ body: { token } }));
+    } catch (err) {
+      const errorMessage = err.message || "Login failed";
+      dispatch(loginFail(errorMessage));
+    }
+  };
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              id="remember-me"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)} // ✅ Mise à jour de l'état
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          {error && <p className="error-message">{error}</p>}
+
+          <button type="submit" className="sign-in-button">
+            Sign In
+          </button>
+        </form>
+      </section>
+    </main>
   );
 }
 
