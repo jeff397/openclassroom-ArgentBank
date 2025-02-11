4a01d5d27ad8264376d30fbad4339d03bea2b892
jeff <jeffdelmotte@gmail.com>
2025-02-11 16:44:13 +0100

    mise en place de la persistance

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,16 +1,32 @@
+import { persistReducer, persistStore, PERSIST } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
 import { loginReducer } from "../slices/loginSlice";
 import userReducer from "../slices/userSlice";
 
+const loginConfig = {
+  key: "login",
+  storage,
+};
+
+const userConfig = {
+  key: "user",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(userConfig, userReducer);
+const persistedLoginReducer = persistReducer(loginConfig, loginReducer);
+
 export const store = configureStore({
   reducer: {
-    login: loginReducer,
-    user: userReducer,
+    login: persistedLoginReducer,
+    user: persistedUserReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
     }),
 });
-
-export default store;
+export const persistedStore = persistStore(store);
